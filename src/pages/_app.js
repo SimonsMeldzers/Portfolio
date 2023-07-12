56d7fdc0c85bceadf58a9008413335d61f4c38e1
Simon <fiewind1@gmail.com>
2023-07-12 13:15:02 +0300

    Installed MUI, stared working on Navbar, changed favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 
+import '@/styles/components/Navbar.css'
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
+import React from "react";
+
+import Navbar from '@/pages/components/Navbar'
 
 export default function Home() {
   return (
     <>
+      <Navbar/>
       <h1>Hi</h1>
     </>
   )
