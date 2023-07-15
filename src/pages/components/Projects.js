1ece06451fe21582b6ff38c6e59b2120a27a2841
Simon <fiewind1@gmail.com>
2023-07-15 19:54:20 +0300

    Finished the projects section

--- src/pages/components/Projects.js
+++ src/pages/components/Projects.js
@@ -1,5 +1,5 @@
 import { Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 
 import reactLogo from '@/img/skills/react.png'
 import nextLogo from '@/img/skills/nextjs.png'
@@ -27,7 +27,10 @@ import smIvona from '@/img/sites/ivona/sm.png'
 import mdIvona from '@/img/sites/ivona/md.png'
 import xlIvona from '@/img/sites/ivona/xl.png'
 
+import linkIcon from '@/img/icons/link.png'
+
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Projects() {
   return (
@@ -38,6 +41,7 @@ export default function Projects() {
                 text="This website was designed for a local realtor company that offers it’s 
                 clients a large variety of real estate. The website needed to be modern, responsive, 
                 and fast, to ensure great user experience, resulting in increased chance of a client."
+                link="https://erker.vercel.app/"
                 skills={
                     [
                         nextLogo,
@@ -57,6 +61,7 @@ export default function Projects() {
             <ProjectsItem 
                 title="Kids’ gift boxes"
                 text="“Soft, clean, and simple” were words that had to be in mind when designing this e-commerce website. The overall esthetic had to be neutral yet bright for a potential client to show interest in buying the products.  "
+                link="https://baby-box.netlify.app/"
                 skills={
                     [
                         nextLogo,
@@ -76,6 +81,7 @@ export default function Projects() {
             <ProjectsItem 
                 title="Custom wooden houses"
                 text="Client wanted a very simple, multilingual, informative website. Although the project ended up being rushed, the final product delivered an easy to navigate webpage, with a unique design for the industry."
+                link="https://www.ininestate.com/"
                 skills={
                     [
                         nextLogo,
@@ -94,6 +100,7 @@ export default function Projects() {
             <ProjectsItem 
                 title="A car rental and repair shop"
                 text="A platforms that allows it’s users to explore the variety of services this company provides, as well as reserve a specific rental. Meanwhile from the admin’s perspective, website allows to add, edit, delete cars, manage reserved cars, and much more."
+                link="https://ivonaplus.netlify.app/"
                 skills={
                     [
                         reactLogo,
@@ -115,13 +122,16 @@ export default function Projects() {
 };
 
 
-const ProjectsItem = ({title, text, skills, sites}) => {
-
+const ProjectsItem = ({title, text, skills, sites, link}) => {
+    const [isHovered, setIsHovered] = useState(false);
     return(
         <div className="p-item">
-            <Typography className='p-title' variant='h3' component='h2'>
-                {title}
-            </Typography>
+            <Link target="_blank" className='p-link' href={link}>
+                <Typography onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className='p-title' variant='h3' component='h2'>
+                { isHovered ? <Image src={linkIcon} className='p-link-icon'/> : null }
+                    {title}
+                </Typography>
+            </Link>
             <Typography> 
                 {text}
             </Typography>
@@ -133,9 +143,9 @@ const ProjectsItem = ({title, text, skills, sites}) => {
             <div className="p-collage">
                 <div className="p-collage-container">
 
-                    <Image src={sites[2]} className="project-xl" alt="erker-xl"/>
-                    <Image src={sites[1]} className="project-md" alt="erker-md"/>
-                    <Image src={sites[0]} className="project-sm" alt="erker-sm"/>
+                    <Image href='/' src={sites[2]} className="project-xl" alt="erker-xl"/>
+                    <Image href='/' src={sites[1]} className="project-md" alt="erker-md"/>
+                    <Image href='/' src={sites[0]} className="project-sm" alt="erker-sm"/>
 
                     <span className="p-cicrcle-1"></span>
                     <span className="p-cicrcle-2"></span>
